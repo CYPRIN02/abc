43e556f94cd579d59d2923b92d4f79b46d8a1b33
CYPRIN02 <prasoloarivony@gmail.com>
2019-09-19 11:36:22 +0200

    hello fraction

---
+++ coucou.java
@@ -0,0 +1,23 @@
+
+
+public class Fraction {
+	int numerateur;
+	int denominateur;
+	
+	public Fraction(){
+		numerateur = 0;
+		denominateur = 1;
+	}
+	
+	public Fraction(int num, int den){
+		numerateur = num;
+		denominateur = den;
+	}
+	public void add(int number){
+		numerateur += number * denominateur;
+	}
+	public void add(int number){
+		
+	}
+
+}
